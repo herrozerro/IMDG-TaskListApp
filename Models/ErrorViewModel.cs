efb821c2aaa30d260ffa2c74363003c481534b1c
Josiah Bradbury <herrozerro@gmail.com>
2019-01-11 23:32:44 -0600

    initial project commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IMDG_TaskListApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
